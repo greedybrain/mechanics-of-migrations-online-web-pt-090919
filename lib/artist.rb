7d6653a6335c62fb7d1ebdbe85cff45fd7a05431
Naya  Willis <greedybrain18@gmail.com>
2019-11-13 17:00:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,4 @@
 class Artist < ActiveRecord::Base
   
-  # attr_accessor :name, :genre, :age, :hometown
-      
-  # def initialize(name: name, genre: genre, age: age, hometown: hometown)
-  #   @name = name 
-  #   @genre = genre 
-  #   @age = age
-  #   @hometown = hometown
-  # end
   
 end
